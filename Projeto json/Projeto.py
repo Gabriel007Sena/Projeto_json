ae2506d0d31a2246d068d9daec3498a25efa11ce
Gabriel007Sena <128003145+Gabriel007Sena@users.noreply.github.com>
2023-09-26 22:27:15 -0300

    Update Projeto.py

--- Projeto json/Projeto.py
+++ Projeto json/Projeto.py
@@ -1,111 +1,134 @@
 import json
 
-def carregar_produtos():
+
+def carregar_dados():
     try:
-        with open('produtos.json', 'r') as arquivo:
+        with open('dados.json', 'r') as arquivo:
             return json.load(arquivo)
     except FileNotFoundError:
-        return {}
+        return []
 
-def salvar_produtos(produtos):
-    with open('produtos.json', 'w') as arquivo:
-        json.dump(produtos, arquivo, indent=4)
 
-def inserir_produto(produtos):
+def salvar_dados(dados):
+    with open('dados.json', 'w') as arquivo:
+        json.dump(dados, arquivo, indent=4)
+
+def inserir_produto(dados):
     codigo = input("Digite o código do produto: ")
-    if codigo in produtos:
-        print("Código de produto já existe.")
-    else:
-        nome = input("Digite o nome do produto: ")
-        preco = float(input("Digite o preço do produto: "))
-        disponivel = True  
-        produtos[codigo] = {
-            "nome": nome,
-            "quantidade": 0,
-            "preco": preco,
-            "disponivel": disponivel
-        }
-        print("Produto adicionado com sucesso.")
+    nome = input("Digite o nome do produto: ")
+    preco = float(input("Digite o preço do produto: "))
+    
+    novo_produto = {
+        'codigo': codigo,
+        'nome': nome,
+        'preco': preco
+    }
+    
+    for produto in dados:
+        if produto['codigo'] == codigo:
+            print("Erro: Código de produto duplicado. O produto não foi inserido.")
+            return
+    
+    dados.append(novo_produto)
+    salvar_dados(dados)
+    print("Produto inserido com sucesso!")
 
-def consultar_produto(produtos):
-    codigo = input("Digite o código do produto: ")
-    if codigo in produtos:
-        produto = produtos[codigo]
-        print(f"Nome: {produto['nome']}")
-        print(f"Quantidade: {produto['quantidade']}")
-        print(f"Preço: {produto['preco']}")
-        print(f"Disponível: {produto['disponivel']}")
-    else:
-        print("Produto não encontrado.")
 
-def consultar_todos_produtos(produtos):
-    if produtos:
-        for codigo, produto in produtos.items():
-            print(f"Código: {codigo}")
-            print(f"Nome: {produto['nome']}")
-            print(f"Quantidade: {produto['quantidade']}")
-            print(f"Preço: {produto['preco']}")
-            print(f"Disponível: {produto['disponivel']}")
-            print("-" * 30)
-    else:
-        print("Não há produtos cadastrados.")
+def consultar_produto(dados):
+    codigo = input("Digite o código do produto que deseja consultar: ")
+    
+    for produto in dados:
+        if produto['codigo'] == codigo:
+            print("Código:", produto['codigo'])
+            print("Nome:", produto['nome'])
+            print("Preço:", produto['preco'])
+            return
+    
+    print("Produto não encontrado.")
 
-def alterar_preco(produtos):
-    codigo = input("Digite o código do produto: ")
-    if codigo in produtos:
-        novo_preco = float(input("Digite o novo preço: "))
-        produtos[codigo]['preco'] = novo_preco
-        print("Preço do produto atualizado com sucesso.")
-    else:
-        print("Produto não encontrado.")
 
-def aplicar_acrescimo_desconto(produtos):
-    percentual = float(input("Digite o percentual de acréscimo (+) ou desconto (-): "))
-    for produto in produtos.values():
+def consultar_todos(dados):
+    if not dados:
+        print("Nenhum produto cadastrado.")
+        return
+    
+    for produto in dados:
+        print("Código:", produto['codigo'])
+        print("Nome:", produto['nome'])
+        print("Preço:", produto['preco'])
+        print()
+
+
+def alterar_preco(dados):
+    codigo = input("Digite o código do produto que deseja alterar o preço: ")
+    novo_preco = float(input("Digite o novo preço: "))
+    
+    for produto in dados:
+        if produto['codigo'] == codigo:
+            produto['preco'] = novo_preco
+            salvar_dados(dados)
+            print("Preço alterado com sucesso!")
+            return
+    
+    print("Produto não encontrado.")
+
+
+def aplicar_acrescimo_desconto(dados):
+    percentual = float(input("Digite o percentual de acréscimo  ou desconto (-): "))
+    
+    for produto in dados:
         produto['preco'] *= (1 + percentual / 100)
-    print("Acréscimo ou desconto aplicado com sucesso.")
+    
+    salvar_dados(dados)
+    print("Acréscimo/desconto aplicado com sucesso!")
 
-def excluir_produto(produtos):
+
+def excluir_produto(dados):
     codigo = input("Digite o código do produto que deseja excluir: ")
-    if codigo in produtos:
-        del produtos[codigo]
-        print("Produto excluído com sucesso.")
-    else:
-        print("Produto não encontrado.")
+    
+    for produto in dados:
+        if produto['codigo'] == codigo:
+            dados.remove(produto)
+            salvar_dados(dados)
+            print("Produto excluído com sucesso!")
+            return
+    
+    print("Produto não encontrado.")
+
 
 def main():
-    produtos = carregar_produtos()
-
+    dados = carregar_dados()
+    
     while True:
-        print("\n=== Menu de Opções ===")
-        print("1. Inserir um novo produto")
-        print("2. Consultar um produto por código")
-        print("3. Consultar todos os produtos")
-        print("4. Alterar o preço de um determinado produto")
-        print("5. Aplicar um acréscimo ou desconto em todos os produtos")
-        print("6. Excluir um registro de produto")
-        print("7. Sair do programa")
-
+        print("Menu de Opções:")
+        print("1. Inserir um novo produto: ")
+        print("2. Consultar um produto por código: ")
+        print("3. Consultar todos os produtos: ")
+        print("4. Alterar o preço de um determinado produto: ")
+        print("5. Aplicar um acréscimo ou desconto em todos os produtos: ")
+        print("6. Excluir um registro de produto: ")
+        print("7. Sair do programa: ")
+        
         opcao = input("Escolha uma opção: ")
-
+        
         if opcao == '1':
-            inserir_produto(produtos)
+            inserir_produto(dados) 
         elif opcao == '2':
-            consultar_produto(produtos)
+            consultar_produto(dados) 
         elif opcao == '3':
-            consultar_todos_produtos(produtos)
+            consultar_todos(dados) 
         elif opcao == '4':
-            alterar_preco(produtos)
+            alterar_preco(dados)
         elif opcao == '5':
-            aplicar_acrescimo_desconto(produtos)
+            aplicar_acrescimo_desconto(dados) 
         elif opcao == '6':
-            excluir_produto(produtos)
+            excluir_produto(dados) 
         elif opcao == '7':
-            salvar_produtos(produtos)
+            salvar_dados(dados) 
             print("Programa encerrado.")
             break
         else:
             print("Opção inválida. Tente novamente.")
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
     main()
